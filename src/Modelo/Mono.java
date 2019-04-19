67f7ac6db31440b86a093e53b7d756f79fe309ee
Wilfred Rafael Alvarez <wilfred9099@gmail.com>
2019-04-19 12:39:23 -0600

    Commit desde el IDE

---
+++ src/Modelo/Mono.java
@@ -0,0 +1,34 @@
+package Modelo;
+
+/**
+ *
+ * @author wilfw
+ */
+public class Mono {
+
+    private String nombre;
+    private String edad;
+
+    public Mono(String nombre, String edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+    
+}
